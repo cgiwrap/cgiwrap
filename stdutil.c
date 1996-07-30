66337c6d1a3d62c7b99e846119f56ee9db2a91a5
Nathan Neulinger <nneul@neulinger.org>
1996-07-30 02:23:24 +0000

    Added cvs log to source.

--- stdutil.c
+++ stdutil.c
@@ -1,5 +1,11 @@
 static char *rcsid="$Id$";
 
+/* CVS Log: $Log$
+/* CVS Log: Revision 1.5  1996/07/30 02:23:24  nneul
+/* CVS Log: Added cvs log to source.
+/* CVS Log: */
+
+
 /**
  **  File: stdutil.c
  **  Purpose: Home grown alternatives for routines not in standard library
